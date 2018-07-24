d7148607a7c05cf69954692b91256bbdb4ce148f
Steve Vasta <svasta@llu.edu>
2018-07-24 16:56:11 -0700

    End-of-the-day checkin . . .

--- server-vb/script/base.sh
+++ server-vb/script/base.sh
@@ -4,7 +4,7 @@
 # file name:     base.sh
 # author:        Steve Vasta
 # created:       February 21, 2017
-# modified:      May 25, 2018
+# modified:      July 24, 2018
 # description:   Customizing an otherwise generic build.
 # note 1:        Assuming file provisioners already put stuff in /tmp.
 
@@ -27,53 +27,29 @@ yum -y install epel-release
 #-------------------------------------------------------------------------------
 # Download prebuilt software to /tmp/prebuilt
 #echo "==> Extract prebuilt software"
-#git clone /git/centos/7/build/prebuilt.git /tmp/prebuilt
+git clone /git/centos/$OS_MAJOR_VERSION/build/prebuilt.git /tmp/prebuilt
+wget https://yum.puppetlabs.com/puppet5/puppet5-release-el-$OS_MAJOR_VERSION.noarch.rpm
+rpm -iv ./puppet5-release-el-$OS_MAJOR_VERSION.noarch.rpm
 
 #-------------------------------------------------------------------------------
-# Install 'most'
+# Install 'most' and 'puppet-agent' . . .
 yum -y update
-yum -y install most
+yum -y install most puppet-agent
 
-#-------------------------------------------------------------------------------
-# Install 'puppet-agent'
-#echo "==> Installing puppet-agent"
-#yum --nogpgcheck localinstall /tmp/prebuilt/puppet-agent-5.5.1-1.el7.$DISTRIB_DIR.rpm -y
-#
 #-------------------------------------------------------------------------------
 # Install 'r10k'
 #
-#echo "==> Installing r10k"
-#
-#if [ -d gems ]; then
-#  rm -rf gems
-#fi
-#
-#tar xpfj /tmp/prebuilt/r10k-gems.tar.bz2
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/colored-1.2.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/cri-2.6.1.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/log4r-1.1.10.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/multi_json-1.13.1.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/minitar-0.6.1.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/multipart-post-2.0.0.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/faraday-0.13.1.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/faraday_middleware-0.12.2.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/semantic_puppet-1.0.2.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/puppet_forge-2.2.9.gem
-#/opt/puppetlabs/puppet/bin/gem install --local ./gems/r10k-2.6.2.gem
-#
-#rm -rf ./gems
-#
-#echo "==> Cleaning up prebuilt software . . ."
-#rm -rf /tmp/prebuilt
+echo "==> Installing r10k"
+/opt/puppetlabs/puppet/bin/gem install r10k
 
 #-------------------------------------------------------------------------------
 # Setup puppet configuration to finish build/conf of our new machine . . .
-#git clone /git/centos/7/build/puppet.git /tmp/puppet
-#export PUPPETFILE=/tmp/puppet/Puppetfile ; export PUPPETFILE_DIR=/tmp/puppet/modules ; /opt/puppetlabs/puppet/bin/r10k puppetfile install
+git clone /git/centos/$OS_MAJOR_VERSION/build/puppet.git /tmp/puppet
+export PUPPETFILE=/tmp/puppet/Puppetfile ; export PUPPETFILE_DIR=/tmp/puppet/modules ; /opt/puppetlabs/puppet/bin/r10k puppetfile install
 
 #-------------------------------------------------------------------------------
 # Start puppet . . .
-#/opt/puppetlabs/puppet/bin/puppet apply --modulepath=/tmp/puppet/modules /tmp/puppet/manifests/site.pp
+/opt/puppetlabs/puppet/bin/puppet apply --modulepath=/tmp/puppet/modules /tmp/puppet/manifests/site.pp
 
 #-------------------------------------------------------------------------------
 # Cleanup the local /git directory . . .
--- server-vb/script/gitbase.sh
+++ server-vb/script/gitbase.sh
@@ -4,14 +4,16 @@
 # file name:     gitbase.sh
 # author:        Steve Vasta
 # created:       May 23, 2018
-# modified:      May 25, 2018
+# modified:      July 24, 2018
 # description:   Create the base directory for a local 'git' pool.
 
 set -eux
 
+export OS_MAJOR_VERSION=`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`
+
 #-------------------------------------------------------------------------------
 # Create the directories . . .
-mkdir -p /git/centos/7
+mkdir -p /git/centos/$OS_MAJOR_VERSION
 chmod -R 0777 /git
 
 #-------------------------------------------------------------------------------
